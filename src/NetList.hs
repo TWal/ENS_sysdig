97eac116d3fdb879ff838eb9412347d8d524f042
lucas8 <luc.linux@mailoo.org>
2017-01-15 00:57:30 +0100

    Implement a Monad-free DSL

--- src/NetList.hs
+++ src/NetList.hs
@@ -2,17 +2,17 @@
 module NetList where
 import Data.Int
 import Data.List
+import Data.Maybe
 import qualified Control.Monad.Trans.State.Strict as S
 import qualified Control.Monad as M
 
 -- A variable is a size and a computing tree
--- The string uniquely identify the var
-type Var = (String,Int8,TVar)
+type Var = (Int8,TVar)
 data TVar =
-    Einput
+    Einput String
   | Econst Int
   | Earg Var
-  | Ereg String
+  | Ereg Var
   | Enot Var
   | Eor Var Var
   | Eand Var Var
@@ -25,33 +25,38 @@ data TVar =
   | Eslice Int8 Int8 Var
   | Eselect Int8 Var
   deriving (Show)
-
-
-
-make_gen :: String -> [String]
-make_gen p = map (\c -> "__" ++ p ++ "_" ++ show c) [1..]
-
-type VarMonad = S.StateT [String] (Either String)
-nlabel :: VarMonad String
-nlabel = do
-    c <- S.get
-    S.put $ tail c
-    return $ head c
+type LVar = (String,Int8,TLVar)
+data TLVar =
+    LEinput
+  | LEconst Int
+  | LEarg LVar
+  | LEreg String
+  | LEnot LVar
+  | LEor LVar LVar
+  | LEand LVar LVar
+  | LExor LVar LVar
+  | LEnand LVar LVar
+  | LEmux LVar LVar LVar
+  | LErom Int8 Int8 LVar
+  | LEram Int8 Int8 LVar LVar LVar LVar
+  | LEconcat LVar LVar
+  | LEslice Int8 Int8 LVar
+  | LEselect Int8 LVar
+  deriving (Show)
 
 size :: Var -> Int8
-size (_,s,_) = s
-label :: Var -> String
+size (s,_) = s
+label :: LVar -> String
 label (l,_,_) = l
 
-test_var_eq :: Int -> Var -> Var -> Bool
-test_var_eq 0 _ _ = False
-test_var_eq n (l1,s1,tv1) (l2,s2,tv2) = if l1 == l2 then True  else
-                                        if s1 /= s2 then False else
+test_var_eq :: Var -> Var -> Bool
+test_var_eq (s1,tv1) (s2,tv2) = if s1 /= s2 then False else
  case (tv1,tv2) of
-  (Econst i,Econst j) -> i == j
-  (Earg x1,Earg x2)   -> test x1 x2
-  (Ereg x1,Ereg x2)   -> x1 == x2
-  (Enot x1,Enot x2)   -> test x1 x2
+  (Einput s1,Einput s2)           -> s1 == s2
+  (Econst i,Econst j)             -> i == j
+  (Earg x1,Earg x2)               -> test x1 x2
+  (Ereg x1,Ereg x2)               -> False
+  (Enot x1,Enot x2)               -> test x1 x2
   (Eor x1 x2, Eor y1 y2)          -> test x1 y1 && test x2 y2
   (Eand x1 x2, Eand y1 y2)        -> test x1 y1 && test x2 y2
   (Exor x1 x2, Exor y1 y2)        -> test x1 y1 && test x2 y2
@@ -65,66 +70,72 @@ test_var_eq n (l1,s1,tv1) (l2,s2,tv2) = if l1 == l2 then True  else
   (Eslice i1 i2 x,Eslice j1 j2 y) -> i1 == j1 && i2 == j2 && test x y
   (Eselect i x, Eselect j y)      -> i == j && test x y
   _                               -> False
- where test = test_var_eq $ n - 1
+ where test = test_var_eq
 data EqVar = EqVar Var deriving (Show)
 fromv :: Var -> EqVar
 fromv v = EqVar v
 instance Eq EqVar where
-    (EqVar x) == (EqVar y) = test_var_eq 10000 x y
+    (EqVar x) == (EqVar y) = test_var_eq x y
 
-remove_double :: [String] -> [Var] -> [Var]
-remove_double secure l = fst $ foldl (\(l,mp) -> \v -> let (nv,nmp) = remove_double' v mp in (nv : l, nmp)) ([],[]) l
-  where remove_double' :: Var -> [(EqVar,Var)] -> (Var,[(EqVar,Var)])
-        remove_double' x@(l,s,tv) mp = if l `elem` secure then ((l,s,ntv),(fromv x,(l,s,ntv)) : nmp)
-            else case lookup (fromv x) mp of
-             Just t    -> (t,mp)
-             Nothing   -> ((l,s,ntv),(fromv x,(l,s,ntv)) : nmp)
-         where (ntv,nmp) = case tv of
-                Einput      -> (tv,mp)
-                Earg v      -> let (v',mp') = remove_double' v mp in (Earg v', mp')
-                Econst i    -> (tv,mp)
-                Ereg s      -> (tv,mp)
-                Enot v      -> let (v',mp') = remove_double' v mp in (Enot v', mp')
-                Eand v1 v2  -> let (v1',mp')  = remove_double' v1 mp in
-                               let (v2',mp'') = remove_double' v2 mp' in
-                               (Eand v1' v2', mp'')
-                Eor  v1 v2  -> let (v1',mp')  = remove_double' v1 mp in
-                               let (v2',mp'') = remove_double' v2 mp' in
-                               (Eor v1' v2', mp'')
-                Exor v1 v2  -> let (v1',mp')  = remove_double' v1 mp in
-                               let (v2',mp'') = remove_double' v2 mp' in
-                               (Exor v1' v2', mp'')
-                Enand v1 v2 -> let (v1',mp')  = remove_double' v1 mp in
-                               let (v2',mp'') = remove_double' v2 mp' in
-                               (Enand v1' v2', mp'')
+remove_double :: [Var] -> [Var] -> ([String],[LVar])
+remove_double out l =
+    let (tree,mp,_) = foldl (\(l,mp,c) -> \v -> let (nv,nmp,nc) = remove_double' v mp c in (nv : l, nmp, nc)) ([],[],0) l
+    in (map (fst3 . fromJust . ((flip lookup) mp) . fromv) out, tree)
+  where fst3 (a,_,_) = a
+        remove_double' :: Var -> [(EqVar,LVar)] -> Int -> (LVar,[(EqVar,LVar)],Int)
+        remove_double' x@(s,tv) mp c = case lookup (fromv x) mp of
+             Just t    -> (t,mp,c)
+             Nothing   -> ((l,s,ntv), (fromv x,(l,s,ntv)) : nmp, nc)
+         where (ntv,nmp,nc) = case tv of
+                Einput _    -> (LEinput,mp,c)
+                Earg v      -> let (v',mp',c') = remove_double' v mp (c+1) in (LEarg v', mp', c')
+                Econst i    -> (LEconst i,mp,c+1)
+                Ereg v      -> let (v',mp',c') = remove_double' v mp (c+1)
+                               in (LEreg (label v'),mp',c')
+                Enot v      -> let (v',mp',c') = remove_double' v mp (c+1) in (LEnot v', mp', c')
+                Eand v1 v2  -> let (v1',mp',c')   = remove_double' v1 mp (c+1) in
+                               let (v2',mp'',c'') = remove_double' v2 mp' c' in
+                               (LEand v1' v2', mp'',c'')
+                Eor  v1 v2  -> let (v1',mp',c')   = remove_double' v1 mp (c+1) in
+                               let (v2',mp'',c'') = remove_double' v2 mp' c' in
+                               (LEor v1' v2', mp'', c'')
+                Exor v1 v2  -> let (v1',mp',c')   = remove_double' v1 mp (c+1) in
+                               let (v2',mp'',c'') = remove_double' v2 mp' c' in
+                               (LExor v1' v2', mp'',c'')
+                Enand v1 v2 -> let (v1',mp',c')   = remove_double' v1 mp (c+1) in
+                               let (v2',mp'',c'') = remove_double' v2 mp' c' in
+                               (LEnand v1' v2', mp'',c'')
                 Econcat v1 v2 ->
-                               let (v1',mp')  = remove_double' v1 mp in
-                               let (v2',mp'') = remove_double' v2 mp' in
-                               (Econcat v1' v2', mp'')
+                               let (v1',mp',c')   = remove_double' v1 mp (c+1) in
+                               let (v2',mp'',c'') = remove_double' v2 mp' c' in
+                               (LEconcat v1' v2', mp'',c'')
                 Emux v1 v2 v3 ->
-                               let (v1',mp')   = remove_double' v1 mp in
-                               let (v2',mp'')  = remove_double' v2 mp' in
-                               let (v3',mp''') = remove_double' v3 mp'' in
-                               (Emux v1' v2' v3', mp''')
+                               let (v1',mp',c')     = remove_double' v1 mp (c+1) in
+                               let (v2',mp'',c'')   = remove_double' v2 mp' c' in
+                               let (v3',mp''',c''') = remove_double' v3 mp'' c'' in
+                               (LEmux v1' v2' v3', mp''',c''')
                 Erom i1 i2 v ->
-                               let (v',mp') = remove_double' v mp in (Erom i1 i2 v', mp')
+                               let (v',mp',c') = remove_double' v mp (c+1)
+                               in (LErom i1 i2 v', mp', c')
                 Eram i1 i2 v1 v2 v3 v4 ->
-                               let (v1',mp1) = remove_double' v1 mp in
-                               let (v2',mp2) = remove_double' v2 mp1 in
-                               let (v3',mp3) = remove_double' v3 mp2 in
-                               let (v4',mp4) = remove_double' v4 mp3 in
-                               (Eram i1 i2 v1' v2' v3' v4', mp4)
+                               let (v1',mp1,c1) = remove_double' v1 mp  (c+1) in
+                               let (v2',mp2,c2) = remove_double' v2 mp1 c1 in
+                               let (v3',mp3,c3) = remove_double' v3 mp2 c2 in
+                               let (v4',mp4,c4) = remove_double' v4 mp3 c3 in
+                               (LEram i1 i2 v1' v2' v3' v4', mp4, c4)
                 Eslice i1 i2 v ->
-                               let (v',mp') = remove_double' v mp in (Eslice i1 i2 v', mp')
-                Eselect i v -> let (v',mp') = remove_double' v mp in (Eselect i v', mp')
-
+                               let (v',mp',c') = remove_double' v mp (c+1)
+                               in (LEslice i1 i2 v', mp',c')
+                Eselect i v -> let (v',mp',c') = remove_double' v mp (c+1)
+                               in (LEselect i v', mp', c')
+               l = case tv of
+                    Einput nm -> nm
+                    _         -> "__" ++ show c
 
 writeNetlist :: [Var] -> [Var] -> [String] -> String
 writeNetlist cmps vs other_outputs =
-    let outputs = map label vs in
-    let filter = remove_double outputs in
-    -- let filter = id in
-    let usedv = filter $ cmps ++ vs in
+    let filter = remove_double vs in
+    let (outputs,usedv) = filter $ cmps ++ vs in
     let (_,inputs,vars,eqs) = foldl (flip rdfs) ([],[],[],[]) $ usedv in
        "INPUT "    ++ sepBy ", " id       inputs
     ++ "\nOUTPUT " ++ sepBy ", " id       (outputs ++ other_outputs)
@@ -132,29 +143,29 @@ writeNetlist cmps vs other_outputs =
     ++ "\nIN\n"    ++ sepBy "\n" id       eqs
  where rdfs v@(l,s,_) x@(sns,ai,av,ae) = if elem l sns then x
                                          else dfs v (l:sns,ai,(l,s):av,ae)
-       dfs (l,s,Einput) (sns,ai,av,ae) = (sns,l:ai,av,ae)
-       dfs (l,s,Earg v) (sns,ai,av,ae) = rdfs v
+       dfs (l,s,LEinput) (sns,ai,av,ae) = (sns,l:ai,av,ae)
+       dfs (l,s,LEarg v) (sns,ai,av,ae) = rdfs v
            (sns,ai,av,(l ++ " = " ++ label v):ae)
-       dfs (l,s,Econst i) (sns,ai,av,ae) =
+       dfs (l,s,LEconst i) (sns,ai,av,ae) =
            (sns,ai,av,(l ++ " = " ++ show i):ae)
-       dfs (l,s,Ereg lb) (sns,ai,av,ae) =
+       dfs (l,s,LEreg lb) (sns,ai,av,ae) =
            (sns,ai,av,(l ++ " = REG " ++ lb):ae)
-       dfs (l,s,Enot v) (sns,ai,av,ae) = rdfs v
+       dfs (l,s,LEnot v) (sns,ai,av,ae) = rdfs v
            (sns,ai,av,(l ++ " = NOT " ++ label v):ae)
-       dfs (l,s,Eor v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
+       dfs (l,s,LEor v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
            (sns,ai,av,(l ++ " = OR " ++ label v1 ++ " " ++ label v2):ae)
-       dfs (l,s,Eand v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
+       dfs (l,s,LEand v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
            (sns,ai,av,(l ++ " = AND " ++ label v1 ++ " " ++ label v2):ae)
-       dfs (l,s,Exor v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
+       dfs (l,s,LExor v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
            (sns,ai,av,(l ++ " = XOR " ++ label v1 ++ " " ++ label v2):ae)
-       dfs (l,s,Enand v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
+       dfs (l,s,LEnand v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
            (sns,ai,av,(l ++ " = NAND " ++ label v1 ++ " " ++ label v2):ae)
-       dfs (l,s,Emux v1 v2 v3) (sns,ai,av,ae) = rdfs v1 $ rdfs v2 $ rdfs v3
+       dfs (l,s,LEmux v1 v2 v3) (sns,ai,av,ae) = rdfs v1 $ rdfs v2 $ rdfs v3
            (sns,ai,av,(l ++ " = MUX " ++ label v1 ++ " " ++ label v2 ++ " " ++ label v3):ae)
-       dfs (l,s,Erom i1 i2 v) (sns,ai,av,ae) = rdfs v
+       dfs (l,s,LErom i1 i2 v) (sns,ai,av,ae) = rdfs v
            (sns,ai,av,
                (l ++ " = ROM " ++ show i1 ++ " " ++ show i2 ++ " " ++ label v):ae)
-       dfs (l,s,Eram i1 i2 v1 v2 v3 v4) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
+       dfs (l,s,LEram i1 i2 v1 v2 v3 v4) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
                                                        $ rdfs v3 $ rdfs v4
            (sns,ai,av,(l ++ " = RAM " ++ show i1
                          ++ " "       ++ show i2
@@ -162,12 +173,12 @@ writeNetlist cmps vs other_outputs =
                          ++ " "       ++ label v2
                          ++ " "       ++ label v3
                          ++ " "       ++ label v4):ae)
-       dfs (l,s,Econcat v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
+       dfs (l,s,LEconcat v1 v2) (sns,ai,av,ae) = rdfs v1 $ rdfs v2
            (sns,ai,av,(l ++ " = CONCAT " ++ label v2 ++ " " ++ label v1):ae)
-       dfs (l,s,Eslice i1 i2 v) (sns,ai,av,ae) = rdfs v
+       dfs (l,s,LEslice i1 i2 v) (sns,ai,av,ae) = rdfs v
            (sns,ai,av,(l ++ " = SLICE " ++ show i1 ++ " " ++ show i2
                          ++ " " ++ label v):ae)
-       dfs (l,s,Eselect i v) (sns,ai,av,ae) = rdfs v
+       dfs (l,s,LEselect i v) (sns,ai,av,ae) = rdfs v
            (sns,ai,av,(l ++ " = SELECT " ++ show i ++ " " ++ label v):ae)
        sepBy c s (h1:h2:t) = s h1 ++ c ++ sepBy c s (h2:t)
        sepBy _ s (h:[])    = s h
@@ -175,83 +186,67 @@ writeNetlist cmps vs other_outputs =
        show_var (l,s) = if s == 1 then l
                         else l ++ " : " ++ show s
 
-runVM :: [String] -> VarMonad a -> a
-runVM g m = case S.evalStateT m g of
-    Right x -> x
-    Left  e -> error e
+input :: String -> Int8 -> Var
+input l s = (s,Einput l)
 
-singleton :: Var -> VarMonad [Var]
-singleton v = return [v]
-outputs :: [VarMonad Var] -> VarMonad [Var]
-outputs vs = M.sequence vs
+reg :: Var -> Var
+reg v = (size v, Ereg v)
 
-create :: (Int8,TVar) -> VarMonad Var
-create (s,t) = nlabel >>= \l -> return (l,s,t)
+notv :: Var -> Var
+notv v = (size v, Enot v)
 
-input :: String -> Int8 -> VarMonad Var
-input l s = return (l,s,Einput)
-
-copy :: Var -> VarMonad Var
-copy (_,s,t) = create (s,t)
-
-reg :: Var -> VarMonad Var
-reg v = create (size v, Ereg $ label v)
-
-notv :: Var -> VarMonad Var
-notv v = create (size v, Enot v)
-
-(&:) :: Var -> Var -> VarMonad Var
-v1 &: v2 = if n1 == n2 then create (n1, Eand v1 v2)
-           else fail "Anding differently sized variables"
+(&:) :: Var -> Var -> Var
+v1 &: v2 = if n1 == n2 then (n1, Eand v1 v2)
+           else error "Anding differently sized variables"
  where n1 = size v1
        n2 = size v2
 
-(|:) :: Var -> Var -> VarMonad Var
-v1 |: v2 = if n1 == n2 then create (n1, Eor v1 v2)
-           else fail "Oring differently sized variables"
+(|:) :: Var -> Var -> Var
+v1 |: v2 = if n1 == n2 then (n1, Eor v1 v2)
+           else error "Oring differently sized variables"
  where n1 = size v1
        n2 = size v2
 
-(^:) :: Var -> Var -> VarMonad Var
-v1 ^: v2 = if n1 == n2 then create (n1, Exor v1 v2)
-           else fail "Xoring differently sized variables"
+(^:) :: Var -> Var -> Var
+v1 ^: v2 = if n1 == n2 then (n1, Exor v1 v2)
+           else error "Xoring differently sized variables"
  where n1 = size v1
        n2 = size v2
 
-(!:) :: Var -> Var -> VarMonad Var
-v1 !: v2 = if n1 == n2 then create (n1, Enand v1 v2)
-           else fail "Nanding differently sized variables"
+(!:) :: Var -> Var -> Var
+v1 !: v2 = if n1 == n2 then (n1, Enand v1 v2)
+           else error "Nanding differently sized variables"
  where n1 = size v1
        n2 = size v2
 
-(<:) :: Var -> (Var, Var) -> VarMonad Var
-v1 <: (v2,v3) = if n1 /= 1 then fail "Muxing on nap"
-              else if n2 /= n3 then fail "Muxing two differently sized variables"
-              else create (n2, Emux v1 v2 v3)
+(<:) :: Var -> (Var, Var) -> Var
+v1 <: (v2,v3) = if n1 /= 1 then error "Muxing on nap"
+              else if n2 /= n3 then error "Muxing two differently sized variables"
+              else (n2, Emux v1 v2 v3)
  where n1 = size v1
        n2 = size v2
        n3 = size v3
 
 -- Check sizes of variables
-rom :: Int8 -> Int8 -> Var -> VarMonad Var
-rom i1 i2 v = create (i2, Erom i1 i2 v)
-ram :: Int8 -> Int8 -> Var -> Var -> Var -> Var -> VarMonad Var
-ram i1 i2 v1 v2 v3 v4 = create (i2, Eram i1 i2 v1 v2 v3 v4)
+rom :: Int8 -> Int8 -> Var -> Var
+rom i1 i2 v = (i2, Erom i1 i2 v)
+ram :: Int8 -> Int8 -> Var -> Var -> Var -> Var -> Var
+ram i1 i2 v1 v2 v3 v4 = (i2, Eram i1 i2 v1 v2 v3 v4)
 
-(-:) :: Var -> Var -> VarMonad Var
-v1 -: v2 = create (size v1 + size v2, Econcat v1 v2)
+(-:) :: Var -> Var -> Var
+v1 -: v2 = (size v1 + size v2, Econcat v1 v2)
 
-(!!:) :: Var -> (Int8,Int8) -> VarMonad Var
-v !!: (i1,i2) = if i1 > i2 then fail "Invalid range for splice"
-                else if i2 >= size v then fail "Too big bound on splice"
-                else create (i2 - i1 + 1, Eslice i1 i2 v)
+(!!:) :: Var -> (Int8,Int8) -> Var
+v !!: (i1,i2) = if i1 > i2 then error "Invalid range for splice"
+                else if i2 >= size v then error "Too big bound on splice"
+                else (i2 - i1 + 1, Eslice i1 i2 v)
 
-(@:) :: Var -> Int8 -> VarMonad Var
-v @: i = if i >= size v then fail "Select indice too big"
-         else create (1, Eselect i v)
+(@:) :: Var -> Int8 -> Var
+v @: i = if i >= size v then error "Select indice too big"
+         else (1, Eselect i v)
 
-constV :: Int8 -> Int -> VarMonad Var
-constV s i = create (s, Econst i)
+constV :: Int8 -> Int -> Var
+constV s i = (s, Econst i)
 
 type Netlist = ([Var],[Var],[String]) -- to be calculated variables,out varables names, out names for debug
 
--- src/Utility.hs
+++ src/Utility.hs
@@ -7,88 +7,70 @@ import Control.Monad
 import Data.Char (ord)
 
 -- Returns a bit true if n4 (a 4-sized nap) holds the value i
-select4_bit :: Int8 -> Var -> VarMonad Var
-select4_bit i n4 = do
-    b0  <- n4 @: 0
-    b1  <- n4 @: 1
-    b2  <- n4 @: 2
-    b3  <- n4 @: 3
-    bn1 <- select2_bit i1 b1 b0
-    bn2 <- select2_bit i2 b3 b2
-    r   <- bn1 &: bn2
-    return r
- where i1 = i `mod` 4
+select4_bit :: Int8 -> Var -> Var
+select4_bit i n4 = r
+ where b0 = n4 @: 0
+       b1  = n4 @: 1
+       b2  = n4 @: 2
+       b3  = n4 @: 3
+       bn1 = select2_bit i1 b1 b0
+       bn2 = select2_bit i2 b3 b2
+       r   = bn1 &: bn2
+       i1 = i `mod` 4
        i2 = (i `div` 4) `mod` 4
 
 -- Returns true is v1v0 holds the value of i (v0 is the LSB)
-select2_bit :: Int8 -> Var -> Var -> VarMonad Var
+select2_bit :: Int8 -> Var -> Var -> Var
 select2_bit i v1 v0 = case ((i `div` 2) `mod` 2,i `mod` 2) of
-    (0,0) -> do r0 <- notv v0
-                r1 <- notv v1
-                r  <- r0 &: r1
-                return r
-    (0,1) -> do r1 <- notv v1
-                r  <- r1 &: v0
-                return r
-    (1,0) -> do r0 <- notv v0
-                r  <- v1 &: r0
-                return r
-    (1,1) -> do r  <- v0 &: v1
-                return r
+    (0,0) -> (notv v0) &: (notv v1)
+    (0,1) -> (notv v1) &: v0
+    (1,0) -> (notv v0) &: v1
+    (1,1) -> v0 &: v1
 
 -- Simple adder between three bits
-simple_adder :: Var -> Var -> Var -> VarMonad (Var, Var)
-simple_adder c x y = do
-    r' <- c ^: x
-    r  <- r' ^: y
-    c'   <- x &: y
-    c''  <- x |: y
-    c''' <- c &: c''
-    rc   <- c' |: c'''
-    return (r,rc)
+simple_adder :: Var -> Var -> Var -> (Var, Var)
+simple_adder c x y = (r,rc)
+ where r'   = c ^: x
+       r    = r' ^: y
+       c'   = x &: y
+       c''  = x |: y
+       c''' = c &: c''
+       rc   = c' |: c'''
 
-full_adder :: Int8 -> Var -> Var -> VarMonad (Var,Var)
-full_adder 1 x y = do
-    x' <- x @: 0
-    y' <- y @: 0
-    c  <- constV 1 0
-    simple_adder c x' y'
-full_adder n x y = do
-    x' <- x @: (n - 1)
-    y' <- y @: (n - 1)
-    (r',c') <- full_adder (n-1) x y
-    (r,c)   <- simple_adder c' x' y'
-    res <- r -: r'
-    return (res,c)
+full_adder :: Int8 -> Var -> Var -> (Var,Var)
+full_adder 1 x y = simple_adder c x' y'
+ where x' = x @: 0
+       y' = y @: 0
+       c  = constV 1 0
+full_adder n x y = (res,c)
+ where (r',c') = full_adder (n-1) x y
+       (r,c)   = simple_adder c' (x @: (n-1)) (y @: (n-1))
+       res     = r -: r'
 
-long_bin :: (Var -> Var -> VarMonad Var) -> [Var] -> VarMonad Var
-long_bin b [] = fail "long_bin on empty list"
-long_bin b l  = foldM b (head l) $ tail l
+long_bin :: (Var -> Var -> Var) -> [Var] -> Var
+long_bin b [] = error "long_bin on empty list"
+long_bin b l  = foldr b (head l) $ tail l
 
 long_or  = long_bin (|:)
 long_and = long_bin (&:)
 
-nap_bin :: (Var -> Var -> VarMonad Var) -> Var -> VarMonad Var
-nap_bin b n = n @: 0 >>= \d -> foldM (\v -> \i -> n @: i >>= \v2 -> b v v2)
-                                     d [1 .. (s-1)]
+nap_bin :: (Var -> Var -> Var) -> Var -> Var
+nap_bin b n = let d = n @: 0 in  foldr (\i -> \v -> let v2 = n @: i in b v v2)
+                                      d [1 .. (s-1)]
  where s = size n
 
 nap_or = nap_bin (|:)
 nap_and = nap_bin (&:)
 
 -- long_select2 long_select4
-long_select :: (Int8 -> Var -> VarMonad Var) -> Var -> [(Int8,Var)] -> VarMonad Var
-long_select f v [(_,rv)] = return rv
-long_select f v ((i,rv) : rvs) = do
-    b   <- f i v
-    nrv <- long_select f v rvs
-    b <: (rv,nrv)
+long_select :: (Int8 -> Var -> Var) -> Var -> [(Int8,Var)] -> Var
+long_select f v [(_,rv)] = rv
+long_select f v ((i,rv) : rvs) = b <: (rv,nrv)
+ where b   = f i v
+       nrv = long_select f v rvs
 
 long_select2 = long_select sel
- where sel i v = do
-        v1 <- v @: 1
-        v0 <- v @: 0
-        select2_bit i v1 v0
+ where sel i v = select2_bit i (v @: 1) (v @: 0)
 long_select4 = long_select select4_bit
 
 binaryToIntegral :: (Integral a) => String -> a
@@ -100,23 +82,10 @@ binaryToInt8 = binaryToIntegral
 binaryToInt :: String -> Int
 binaryToInt = binaryToIntegral
 
-$(genMuxs 10)
+-- $(genMuxs 10)
 
 inputV ::String -> Int8 -> Var
-inputV s i = (s,i,Einput)
+inputV s i = (i,Einput s)
 
-vconstV :: String -> Int8 -> Int -> Var
-vconstV nm s c = (nm, s, Econst c)
+(^-:) nap v = let n = notv nap in  v <: (n,nap)
 
-(^-:) nap v = do
-  n <- notv nap
-  v <: (n,nap)
-
-renameV :: String -> Var -> Var
-renameV name (_,s,t) = (name,s,t)
-
-renameVM :: String -> VarMonad Var -> VarMonad Var
-renameVM name vm = do
-  v <- vm
-  let res = renameV name v
-  return res
