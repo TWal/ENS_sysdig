d970520295b4e3334b7804f3ee041b0d7e27f26d
lucas8 <luc.linux@mailoo.org>
2017-01-15 13:07:00 +0100

    Fix bugs in instruction system

--- src/Instructions.hs
+++ src/Instructions.hs
@@ -11,12 +11,14 @@ instruction_reader ins = do
     func <- ins !!: (12,15)
     return (op,dest,src,func)
 
-pp_register w = constV 1 1 >>= \we -> return $ make_register w we "pp"
-get_pp_register = return $ get_register "pp"
+pp_register :: Var -> VarMonad Var
+pp_register w = constV 1 1 >>= \we -> return $ snd $ make_register w we "pp"
+get_pp_register = return $ get_register "pp" :: VarMonad Var
 
 instruction_system test_func test_src test_dest test_out
-                   alu_res alu_hiw alu_hiwe alu_low alu_lowe
+                   alu_res alu_we alu_hiw alu_hiwe alu_low alu_lowe
                    mem_reading mem_nap mem_esp mem_wsp mem_ret
+                   read_reg write_reg
                    flag_code_out =
  runVM (make_gen "instruction_system") $ do
     c1        <- constV 16 1
@@ -26,12 +28,12 @@ instruction_system test_func test_src test_dest test_out
     pp                 <- get_pp_register
     (pp1,_)            <- full_adder 16 c1 pp
     (pp2,_)            <- full_adder 16 c1 pp1
-    ins                <- rom 16 8 pp
-    val                <- rom 16 8 pp1
+    ins                <- rom 16 16 pp
+    val                <- rom 16 16 pp1
     (op,dest,src,func) <- instruction_reader ins
     is_long            <- op @: 3
     next_pp            <- is_long <: (pp2,pp1)
-    next_ins           <- rom 16 8 next_pp
+    next_ins           <- rom 16 16 next_pp
     next_is_long       <- next_ins @: 3
 
     -- Instruction decoding
@@ -53,19 +55,19 @@ instruction_system test_func test_src test_dest test_out
     is_bin     <- is_test |: ins_alu_bin
     real_func' <- is_test <: (test_func,func)
     real_func  <- ins_call <: (code_push,real_func') -- In case of a call, let the memory system do the push
-    real_src   <- is_test <: (test_src,src)
-    real_dest  <- is_test <: (test_dest,dest)
-    write_hi   <- return alu_hiw
+    real_src   <- is_test <: (test_src,read_reg)
+    real_dest  <- is_test <: (test_dest,write_reg)
+    write_hi   <- copy alu_hiw
     enable_hi  <- alu_hiwe &: ins_alu_un
-    write_lo   <- return alu_low
+    write_lo   <- copy alu_low
     enable_lo  <- alu_lowe &: ins_alu_un
 
     -- Memory system input
     mem_enable <- long_or [ins_mem, ins_mem_offset, ins_call] -- In case of a call, let the memory system do the push
     esp        <- mem_enable &: mem_esp -- Enable sp if the memory system is active and if it says so
-    wsp        <- return mem_wsp
-    addr'      <- mem_reading <: (src,dest) -- If we're reading (ie not writing), the address is in src
-                                            -- otherwise it's in dest
+    wsp        <- copy mem_wsp
+    addr'      <- mem_reading <: (read_reg,write_reg) -- If we're reading (ie not writing), the address is in src
+                                                      -- otherwise it's in dest
     (addr'',_) <- full_adder 16 addr' val -- Add the offset
     addr'''    <- is_long <: (addr'',addr') -- Consider the offset only if the instruction is long
     addr       <- ins_call <: (next_pp,addr''') -- In case of the call, what we write is the address of the
@@ -84,19 +86,20 @@ instruction_system test_func test_src test_dest test_out
     jmpdest     <- jump_on_val <: (val,jmpdest')
 
     -- Registers operations
-    read_cmd  <- return src
-    write_cmd <- return dest
+    read_cmd  <- copy src
+    write_cmd <- copy dest
     reg_data' <- is_alu <: (alu_res,mem_nap)
     reg_data  <- ins_limm <: (val,reg_data')
-    reg_we    <- is_alu |: mem_reading
+    reg_we'   <- is_alu &: alu_we
+    reg_we    <- reg_we' |: mem_reading
 
     -- Update pp
     pp_update <- is_jmp <: (jmpdest,next_pp)
-    pp_register pp_update
+    pp        <- pp_register pp_update
 
     return (is_bin, real_func, real_src, real_dest, write_hi, enable_hi, write_lo, enable_lo,
             wsp, esp, mem_enable, addr,
-            read_cmd, write_cmd, reg_data, reg_we)
+            read_cmd, write_cmd, reg_data, reg_we, pp)
 
 
 
